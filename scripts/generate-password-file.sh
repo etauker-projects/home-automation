5ae269afd43bdf1cc426fb6d0bb56d094eb0269c
Tautvydas Kersulis <etauker@gmail.com>
2022-12-06 00:00:01 +0000

    Improve password input in script

--- scripts/generate-password-file.sh
+++ scripts/generate-password-file.sh
@@ -1,16 +1,28 @@
 #! env bash
 
+read_password () {
+    if [ -z "$MOSQUITTO_PASSWORD" ]; then
+        read -s -p "Password: " password
+        echo
+        read -s -p "Repeat password: " password_repeat
+        echo
+        if [ $password != $password_repeat ]; then
+            echo "Passwords do not match, try again..."
+            read_password
+        fi
+    else 
+        password=$MOSQUITTO_PASSWORD
+    fi
+}
+
 if [ -z "$MOSQUITTO_USERNAME" ]; then
     read -p "Username: " username
 else 
     username=$MOSQUITTO_USERNAME
 fi
 
-if [ -z "$MOSQUITTO_PASSWORD" ]; then
-    read -p "Password: " password
-else 
-    password=$MOSQUITTO_PASSWORD
-fi
+read_password
 
 file="/mosquitto/config/passwd.tmp"
-docker-compose run eclipse-mosquitto /bin/sh -c "touch $file && mosquitto_passwd -b $file $username $password && cat $file && rm $file" > /mosquitto/config/passwd
+temp_file="/mosquitto/config/passwd"
+docker-compose run eclipse-mosquitto /bin/sh -c "touch $file && mosquitto_passwd -b $file $username $password && cat $file && rm $file" > $temp_file
