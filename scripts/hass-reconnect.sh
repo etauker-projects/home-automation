88ff0e5244e936ad4faa4fc144648768b06074c9
Tautvydas Kersulis <etauker@gmail.com>
2024-01-27 01:15:05 +0000

    Increased hass-reconnect sleep time to 60s

--- scripts/hass-reconnect.sh
+++ scripts/hass-reconnect.sh
@@ -2,8 +2,8 @@
 
 echo "[hass-reconnect][step 1]: stopping home-assistant..." && \
 docker compose down home-assistant && \
-echo "[hass-reconnect][step 2]: starting home-assistant-host-mode and waiting 10s..." && \
-((docker compose up home-assistant-host-mode & sleep 10s) > /dev/null 2>&1) && \
+echo "[hass-reconnect][step 2]: starting home-assistant-host-mode and waiting 60s..." && \
+(docker compose up home-assistant-host-mode & sleep 60s) > /dev/null 2>&1) && \
 echo "[hass-reconnect][step 3]: stopping home-assistant-host-mode..." && \
 docker compose down home-assistant-host-mode && \
 echo "[hass-reconnect][step 4]: starting home-assistant..." && \
