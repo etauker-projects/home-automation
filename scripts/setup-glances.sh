f770ce82549539993a2126fa2f02fad2ff9ebdb2
etauker <etauker@gmail.com>
2024-02-09 13:43:16 +0000

    Configure glances

--- scripts/setup-glances.sh
+++ scripts/setup-glances.sh
@@ -1,10 +1,8 @@
 #! env bash
 
 # tools for generating glances password
-sudo apt-get install -y glances
 
 # glances setup
 # set up according to:
 # https://github.com/nicolargo/glances/issues/1040
-glances -s -u glances --password
-sudo mv -T "$HOME/.config/glances/glances.pwd" "./data/glances/passwd"
+# https://glances.readthedocs.io/en/latest/docker.html#option-1
