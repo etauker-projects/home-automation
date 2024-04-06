cf3eda26c19ad3646614fcc8d43a0a2947b197b8
etauker <etauker@gmail.com>
2024-04-06 23:04:16 +0100

    Implement basic MqttConnector

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,51 @@
-import { DiskService } from './disk.service';
+import { MqttConnector } from './mqtt/mqtt-connector';
+// import { DiskService } from './disk.service';
 
-const directories = [
-    '/home/etauker',
-    // '/var/lib/*',
-];
 
-const disk = new DiskService();
-const directory = directories[0];
+// const directories = [
+//     '/home/etauker',
+//     // '/var/lib/*',
+// ];
 
-console.log(`Total size: ${((await disk.getTotalSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
-console.log(`Free space: ${((await disk.getFreeSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
-console.log(`Free space: ${((await disk.getFreeSpacePercentage(directory)).toFixed(2))}%`);
-console.log(`Used space: ${((await disk.getUsedSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
-console.log(`Used space: ${((await disk.getUsedSpacePercentage(directory)).toFixed(2))}%`);
+// const disk = new DiskService();
+// const directory = directories[0];
+
+// console.log(`Total size: ${((await disk.getTotalSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
+// console.log(`Free space: ${((await disk.getFreeSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
+// console.log(`Free space: ${((await disk.getFreeSpacePercentage(directory)).toFixed(2))}%`);
+// console.log(`Used space: ${((await disk.getUsedSpaceBytes(directory) / 1_000_000_000).toFixed(2))}GB`);
+// console.log(`Used space: ${((await disk.getUsedSpacePercentage(directory)).toFixed(2))}%`);
+
+const host = process.env.MQTT_HOST;
+if (!host) {
+    throw new Error('Value not provided for MQTT_HOST');
+}
+
+const username = process.env.MQTT_USER;
+if (!username) {
+    throw new Error('Value not provided for MQTT_USER');
+}
+
+const password = process.env.MQTT_PASSWORD;
+if (!password) {
+    throw new Error('Value not provided for MQTT_PASSWORD');
+}
+
+const connector = new MqttConnector(host, username, password);
+const topic = 'etauker/home-automation';
+connector.subscribe(`${topic}/request`, (payload) => {
+    try {
+        const json = JSON.parse(payload);
+        console.log('Received json value:', json);
+
+        if (json.test === true) {
+            const response = { success: true };
+            console.log('Responding with a json object:', response);
+            connector.publish(`${topic}/response`, JSON.stringify(response));
+        }
+
+    } catch (error) {
+        console.error('Received non-json value:', payload);
+        connector.publish(`${topic}/response`, 'Did not get that, please send json');
+    }
+});
