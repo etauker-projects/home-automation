3ee10ee28b660034dcd59d1ab6bd9832a7d69670
etauker <etauker@gmail.com>
2024-04-06 11:13:05 +0100

    Update hass restart scripts

--- scripts/hass-reconnect.sh
+++ scripts/hass-reconnect.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env
 
-echo "[hass-reconnect][step 1]: stopping home-assistant..." && \
+echo "[hass-reconnect][step 1/5]: stopping home-assistant..." && \
 docker compose down home-assistant && \
-echo "[hass-reconnect][step 2]: starting home-assistant-host-mode and waiting 60s..." && \
-(docker compose up home-assistant-host-mode & sleep 60s) > /dev/null 2>&1) && \
-echo "[hass-reconnect][step 3]: stopping home-assistant-host-mode..." && \
+echo "[hass-reconnect][step 2/5]: starting home-assistant-host-mode and waiting 30s..." && \
+((docker compose up home-assistant-host-mode & sleep 30s) > /dev/null 2>&1) && \
+echo "[hass-reconnect][step 3/5]: stopping home-assistant-host-mode..." && \
 docker compose down home-assistant-host-mode && \
-echo "[hass-reconnect][step 4]: starting home-assistant..." && \
+echo "[hass-reconnect][step 4/5]: starting home-assistant..." && \
 docker compose up -d home-assistant && \
-echo "[hass-reconnect][step 5]: ...done"
+echo "[hass-reconnect][step 5/5]: ...done"
