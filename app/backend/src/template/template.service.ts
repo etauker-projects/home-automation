84c5194ca852593fb43e63b46a1303debb7d2a1b
Tautvydas Kersulis <etauker@gmail.com>
2025-06-17 18:12:35 +0100

    WIP

--- app/backend/src/template/template.service.ts
+++ app/backend/src/template/template.service.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import { readFile, readdir } from 'fs/promises';
+import type { AppConfiguration } from '../app';
 
 // Polyfill __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
@@ -11,27 +12,27 @@ export interface Template {
     content: string;
 }
 
-export interface TemplateSet {
+export interface Module {
     module: string;
     templates: Template[];
     variables: Set<string>;
 }
 
-export class TemplatingService {
+export class TemplateService {
 
     private templatePath: string;
-    private readonly regexString = '\\${\\s*input\\.KEY\\s*}';;
+    private readonly regexString = '\\${\\s*input\\.KEY\\s*}';
 
-    constructor() {
-        this.templatePath = resolve(__dirname, '../data/templates');
+    constructor(appconfig: AppConfiguration) {
+        this.templatePath = appconfig.inputDirectory;
     }
 
-    public async loadModule(moduleId: string): Promise<TemplateSet> {
+    public async loadModule(moduleId: string): Promise<Module> {
         const moduleDirectory = resolve(this.templatePath, moduleId);
         const templatePromises = await readdir(moduleDirectory);
         const variables = new Set<string>();
         const templates: Template[] = [];
-        
+
         const templateFileContent = templatePromises.map(async templateName => {
             const path = resolve(moduleDirectory, templateName);
             const content = await readFile(path, 'utf-8');
@@ -47,7 +48,7 @@ export class TemplatingService {
             templates.push({ path, content });
         });
 
-        const res = await Promise.all(templateFileContent);
+        await Promise.all(templateFileContent);
 
         return {
             module: moduleId,
@@ -56,8 +57,8 @@ export class TemplatingService {
         };
     }
 
-    public async prepareTemplate(template: string, inputs: { [key: string]: string }): Promise<string> {   
-        let replaced = template;     
+    public async prepareTemplate(template: string, inputs: { [key: string]: string }): Promise<string> {
+        let replaced = template;
         Object.keys(inputs).forEach((key) => {
             const regex = new RegExp(this.regexString.replace('KEY', key), 'g');
             console.log(key, regex);
