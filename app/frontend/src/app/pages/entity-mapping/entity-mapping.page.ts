ffe64c6504c192b8cb3aac0cd50932df7cbf6d07
Tautvydas Kersulis <etauker@gmail.com>
2025-06-27 14:01:53 +0100

    Clean up

--- app/frontend/src/app/pages/entity-mapping/entity-mapping.page.ts
+++ app/frontend/src/app/pages/entity-mapping/entity-mapping.page.ts
@@ -1,9 +1,9 @@
-import { parse , stringify } from 'yaml';
+import { parse, stringify } from 'yaml';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from '../../components/table/table.component';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule, type FormControl } from '@angular/forms';
+// import { ActivatedRoute } from '@angular/router';
 import { EditorComponent } from '../../components/editor/editor.component';
 import { NGX_MONACO_EDITOR_CONFIG } from 'ngx-monaco-editor-v2';
 import { DiffEditorComponent } from '../../components/diff-editor/diff-editor.component';
@@ -28,13 +28,21 @@ export class EntityMappingPage {
     private readonly regexString = '\\${\\s*input\\.KEY\\s*}';
 
     form: FormGroup;
-    entities: {
+
+    controls: {
+        id: string;
+        label: string;
+        readonly: boolean;
+        control: FormControl;
+    }[];
+
+    previews: {
         title: string,
         template: string,
         output: string,
-        variables: Set<string>;
-    }[];
+    }[] = [];
 
+    // TODO: fetch from the backend
     template = `
 \${ input.id }_energy_usage_hourly:
     name: \${ input.name } Energy Usage Hourly
@@ -59,113 +67,89 @@ export class EntityMappingPage {
     delta_values: false
     `.trim();
 
-    output = `
-office_desk_plug_energy_usage_hourly:
-    name: Office Desk Plug Energy Usage Hourly
-    source: sensor.office_desk_plug_energy
-    cycle: hourly
-    unique_id: meter.office_desk_plug_energy_usage_hourly
-    offset: 0
-    delta_values: false
-office_desk_plug_energy_usage_daily:
-    name: Office Desk Plug Energy Usage Daily
-    source: sensor.office_desk_plug_energy
-    cycle: daily
-    unique_id: meter.office_desk_plug_energy_usage_daily
-    offset: 0
-    delta_values: false
-office_desk_plug_energy_usage_monthly:
-    name: Office Desk Plug Energy Usage Monthly
-    source: sensor.office_desk_plug_energy
-    cycle: monthly
-    unique_id: meter.office_desk_plug_energy_usage_monthly
-    offset: 0
-    delta_values: false
-    `.trim();
-
     constructor(
-        private route: ActivatedRoute,
+        // private route: ActivatedRoute,
         private formBuilder: FormBuilder,
     ) {
-        this.form = this.formBuilder.group({
-            module: ['power_monitoring', Validators.required],
-            id: [''],
-            sourceEntityId: ['sensor.office_desk_plug', Validators.required],
-            templateSourcePath: ['/power_monitoring/template_sensor/plug.yaml', Validators.required],
-            templateDestinationPath: ['/power_monitoring/template_sensor/office_desk_plug.yaml', Validators.required],
-        });
 
-        this.route.paramMap.subscribe(params => {
+        const variables = this.extractVariables(this.template);
+
+        // TODO: split module and entity info into separate forms
+        this.controls = [
+            // {
+            //     id: 'module',
+            //     label: 'Module',
+            //     readonly: true,
+            //     control: this.formBuilder.control('power_monitoring', Validators.required),
+            // },
+            // {
+            //     id: 'sourceEntityId',
+            //     label: 'Source Entity ID',
+            //     readonly: true,
+            //     control: this.formBuilder.control('sensor.office_desk_plug', Validators.required),
+            // },
+            // {
+            //     id: 'templateSourcePath',
+            //     label: 'Template Source Path',
+            //     readonly: true,
+            //     control: this.formBuilder.control('/power_monitoring/template_sensor/plug.yaml', Validators.required),
+            // },
+            // {
+            //     id: 'templateDestinationPath',
+            //     label: 'Template Destination Path',
+            //     readonly: true,
+            //     control: this.formBuilder.control('/power_monitoring/template_sensor/office_desk_plug.yaml', Validators.required),
+            // },
+        ];
+
+        this.form = this.formBuilder.group([]);
+        variables.forEach(variable => this.appendToForm(variable));
+
+        // this.route.paramMap.subscribe(params => {
             // const moduleId = params.get('moduleId') ?? '';
             // this.form.controls['module'].setValue(moduleId);
 
             // const entityId = params.get('entityId') ?? undefined;
             // this.form.controls['sourceEntityId'].setValue(entityId);
-        });
-
-        const entities = parse(this.template);
-        console.log('Parsed template entities:', entities);
-
-        // TODO: handle case where entities is an array
-        this.entities = Object.keys(entities).map(key => {
-
-            const json: { [key: string]: any } = {};
-            json[key] = entities[key];
-            const node = parse(JSON.stringify(json));
-            const template = stringify(node, { indent: 4 });
-
-            const variables = this.extractVariables(template);
-            const title = this.extractTitle(entities[key], key, variables);
-
-            // TODO: get from user input
-            const variablesMap: { [key: string]: string } = {
-                id: 'office_desk_plug',
-                name: 'Office Desk Plug',
-            };
-
-            return {
-                title: title,
-                template: template,
-                output: this.substitueVariables(template, variablesMap),
-                variables: variables,
-            };
-        });
-    }
-    extractTitle(json: { [key: string]: string }, key: string, variables: Set<string>): string {
-        let name = json['name'] ?? key;
-        variables.forEach(variable => name = name.replaceAll(variable, ''));
-        name = name.startsWith('_') ? name.substring(1) : name;
-        name = name.endsWith('_') ? name.substring(0, name.length-1) : name;
-        return name.trim();
+        // });
     }
 
-    // TODO
-    extractVariables(template: string): Set<string> {
+    private appendToForm(variable: string): void {
+        if (!this.controls.some(control => control.id === variable)) {
+            this.controls.push({
+                id: variable,
+                label: this.toHumanReadable(variable),
+                readonly: false,
+                control: this.formBuilder.control('', Validators.required),
+            });
+        }
+        if (!this.form.contains(variable)) {
+            this.form.addControl(variable, this.formBuilder.control('', Validators.required));
+        }
+    }
 
+    // TODO: move to service
+    private extractTitle(json: { [key: string]: string }, key: string): string {
+        const regex = new RegExp(this.regexString.replace('KEY', '(?<input>[a-zA-Z0-9]+)'), 'g');
+        const name = (json['name'] ?? key).replaceAll(regex, '');
+        return this.toHumanReadable(name);
+    }
 
-
-        // const templateFileContent = templatePromises.map(async templateName => {
-        //     const path = resolve(moduleDirectory, templateName);
-        //     const content = await readFile(path, 'utf-8');
+    // TODO: move to service
+    private extractVariables(template: string): Set<string> {
         const variables = new Set<string>();
         const regex = new RegExp(this.regexString.replace('KEY', '(?<input>[a-zA-Z0-9]+)'), 'g');
         const matches = template.matchAll(regex);
-        console.log('Matches found:', matches, template, regex);
-
         for (const match of matches) {
             if (match.groups?.['input']) {
-                // variables[key] = (match.groups?.['input']);
-                // console.log('Found variable:', match.groups?.['input']);
                 variables.add(match.groups?.['input']);
-
             }
         }
-
         return variables;
-
     }
 
-    substitueVariables(template: string, variables: { [key: string]: string }): string {
+    // TODO: move to service
+    private substitueVariables(template: string, variables: { [key: string]: string }): string {
         let result = template;
         Object.keys(variables).forEach(key => {
             const regex = new RegExp(this.regexString.replace('KEY', key), 'g');
@@ -176,9 +160,36 @@ office_desk_plug_energy_usage_monthly:
 
     onSubmit() {
         if (this.form.valid) {
-            console.log('Form submitted:', this.form.value);
-            // Add logic to handle the new mapping here
-            // this.form.reset();
+            const entities = parse(this.template);
+
+            // TODO: handle case where entities is an array
+            this.previews = Object.keys(entities).map(key => {
+
+                const json: { [key: string]: any } = {};
+                json[key] = entities[key];
+                const node = parse(JSON.stringify(json));
+                const template = stringify(node, { indent: 4 });
+                const title = this.extractTitle(entities[key], key);
+
+                const variables = this.getVariableValues(this.controls);
+
+                return {
+                    title: title,
+                    template: template,
+                    output: this.substitueVariables(template, variables),
+                };
+            });
         }
+    }
+    private getVariableValues(controls: { id: string; label: string; readonly: boolean; control: FormControl; }[]): { [key: string]: string } {
+        return controls.reduce((acc, control) => {
+            acc[control.id] = control.control.value;
+            return acc;
+        }, {} as { [key: string]: string });
+    }
+
+    private toHumanReadable(id: string): string {
+        const withSpaces = id.replace(/_/g, ' ');
+        return withSpaces.substring(0, 1).toUpperCase() + withSpaces.substring(1);
     }
 }
